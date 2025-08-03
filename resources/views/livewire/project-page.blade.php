7744e98b68afcef7d1f89195cd019681d7e5f73c
mindgame800 <mindgame080@gmail.com>
2025-08-03 08:14:18 +0600

    mobile responsive

--- resources/views/livewire/project-page.blade.php
+++ resources/views/livewire/project-page.blade.php
@@ -1,3 +1,33 @@
+<div class="max-w-[1200px] mx-auto">
+    <div>
+        <div class="flex flex-col  cursor-pointer mt-4 w-full ">
+            <div class=" flex flex-col justify-center items-center ">
+                <p
+                    class="text-5xl font-bold flex items-center text-gray-700 mt-4 justify-center
+             text-center">
+                    Our Projects</p>
+                <p class="text-gray-500 text-xl font-semibold  text-center mx-40 mt-4">Check out some of Our
+                    recent work and decide yourself.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="mt-12 flex justify-between gap-5 mb-12">
+        <div class="w-[420px] h-[460px] shadow-md flex flex-col">
+            <div>
+                <img src="{{ asset('storage/images/project/code.jpg') }}" class="w-[420px] h-[236px]" alt="">
+            </div>
+            <div class="flex flex-col gap-4 mt-6 px-6">
+                <p class="text-2xl font-semibold text-gray-800">Web Developement</p>
+                <p class="text-gray-600">Complete redesign of corporate website with
+                    improved UX and modern aesthetics</p>
+                <div class="flex gap-4">
+                    <p class="bg-blue-100 text-blue-800 font-semibold p-[4px] px-2 rounded-xl">Web Developement</p>
+                    <p class="bg-green-100 text-green-800 font-semibold p-[4px] px-2 rounded-xl">Next-js</p>
+                </div>
+                <p class="font-semibold text-blue-600 text-lg">View Case Study <i class="fa-solid fa-arrow-right"
+                        style="color: #3c08d9;"></i></p>
+            </div>
         </div>
         <div class="w-[420px] h-[460px] shadow-md flex flex-col">
             <div>
